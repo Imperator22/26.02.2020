1af12308b7294272441fdda62fc080f9808cde30
Imperator22 <alexey.imperator-II@yandex.ru>
2020-03-02 11:58:24 +0300

    v1.1

--- 1.py
+++ 1.py
@@ -1,14 +1,37 @@
 from tkinter import *
 
-def add_label():
-    Label(root, text="New Label", padx="30", pady="20", fg="#eeeeee", bg="#333333").pack(side=BOTTOM)
+counter = 0
+
+def add_label(event=NONE):
+
+    global counter
+
+    user_text = entry.get()
+    entry.delete(0, END)
+    label_new=Label(root, text=user_text, padx="30", pady="20") 
+    if counter % 2:
+        label_new.configure(fg="#eeeeee", bg="#333333")
+        label_new.pack(side=BOTTOM, fill=X)
+    else:
+        label_new.configure(bg="red", fg="#333333")
+        label_new.pack(side=BOTTOM, fill=X)
+    counter += 1
 
 
 root = Tk()
 root.geometry("300x500")
+root.resizable(False, False)
+root.title("simple tk")
 
 Label(root, text="just a test", padx="30", pady="20").pack(side=BOTTOM)
-Button(root, text="click me!", command=add_label).pack()
+input_frame = Frame(root)
+input_frame.pack()
 
+entry = Entry(input_frame, width="15")
+entry.pack(side=LEFT)
+button = Button(input_frame, text="click me!", command=add_label)
+button.pack(side=LEFT)
+
+button.bind('<Return>', add_label)
 
 root.mainloop()
